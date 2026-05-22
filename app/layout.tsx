f34224769387824d01b0c4a4df8d5304ccdc02e3
tORHANS <GiamonsBang@hotmail.com>
2026-05-22 10:08:25 +0800

    2026-05-22 10:08:25 修复页面水合提示

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,9 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="zh-CN">
+    <html lang="zh-CN" suppressHydrationWarning>
       <body>{children}</body>
     </html>
   );
 }
-
